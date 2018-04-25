874715e439d4789da79b715e01934c7876893cd0
hdevoe <hdevoe@stetson.edu>
2018-04-25 13:33:02 -0400

    presentation

---
+++ cinf301-yii-presentation/basic/models/Country.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: home
+ * Date: 25/04/18
+ * Time: 12:44 PM
+ */
+
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class Country extends ActiveRecord
+{
+}
